2b934edb61c844fff3caaca9ac318215d7aed67f
lasseruttert <110640974+lasseruttert@users.noreply.github.com>
2025-10-20 21:40:54 +0200

    new compare logic, also doesnt find min vehicles on certain problems

--- memetic/utils/compare.py
+++ memetic/utils/compare.py
@@ -6,4 +6,11 @@ def compare(fitness1, num_vehicles1, fitness2, num_vehicles2):
     # * Option 2: care for num vehicles first
     # if num_vehicles1 < num_vehicles2 or (num_vehicles1 == num_vehicles2 and fitness1 < fitness2):
         # return True
+
+    # * Option 3: only care for num vehicles
+    # if num_vehicles1 < num_vehicles2:
+    #     return True
+    # elif fitness1 < fitness2:
+    #     return True
+    
     return False
